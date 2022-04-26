d65e368c61e66f12f90d3a1ef96b391418bed49e
Abel Barrios <barriosabel0@hotmail.com>
2022-04-26 10:29:24 -0300

    Se agrega Unidad

--- apps/materiales/models.py
+++ apps/materiales/models.py
@@ -2,7 +2,7 @@
 from django.db import models
 
 
-class Material(models.Model):
+class Unidad(models.Model):
     descripcion = models.CharField(
         max_length=100, unique=True, blank=False, null=False)
 
@@ -10,8 +10,20 @@ class Material(models.Model):
         return self.descripcion
 
     class Meta:
+        verbose_name_plural = "Unidades"
+
+
+class Material(models.Model):
+    descripcion = models.CharField(
+        max_length=100, unique=True, blank=False, null=False)
+    cantidad = models.DecimalField(
+        decimal_places=2, max_digits=10, null=True, blank=True)
+    unidad = models.ForeignKey(Unidad, default=1, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.descripcion
+
+    class Meta:
         verbose_name_plural = "Materiales"
-
-
 
 # Create your models here.
--- apps/materiales/urls.py
+++ apps/materiales/urls.py
@@ -13,13 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
 
-from .views import home
+from django.urls import path
+
+from .views import listadomaterial, nuevomaterial, editarmaterial
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    path('materiales/', include('apps.materiales.urls')),
+    path('listado/', listadomaterial, name='listado'),
+    path('nuevo/', nuevomaterial, name='nuevo'),
+    path('editar/<int:pk>', editarmaterial, name='editar'),
 ]
