e798eb8836a4124eeffcbf3c28c81fb1f90d0a9c
Abel Barrios <barriosabel0@hotmail.com>
2022-04-26 11:15:14 -0300

    Modificacion archivo de carga material

--- cargamaterial.py
+++ cargamaterial.py
@@ -1,5 +1,5 @@
 
-from apps.materiales.models import Material
+from apps.materiales.models import Material, Unidad
 
 archivo = open("materiales.txt", "r")
 
@@ -9,8 +9,11 @@ materiales = list(dict.fromkeys(materiales))
 
 print("Grabando...")
 
+unidad = Unidad.objects.get(pk=1)
+
+
 for material in materiales:
     if material != "":
-        grabar = Material(descripcion=str(material))
+        grabar = Material(descripcion=str(material), cantidad=0, unidad=unidad)
         grabar.save()
 
